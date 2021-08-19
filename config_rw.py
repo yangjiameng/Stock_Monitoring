a60d472822e605fae76228c5876d9b789b08dd9a
yangjiameng <918021618@qq.com>
2021-08-19 18:20:13 +0800

    配置文件读写模块

--- config_rw.py
+++ config_rw.py
@@ -1,12 +1,28 @@
 import configparser as cf
 
 config = cf.ConfigParser()
-config["DEFAULT"] = {'ServerAliveInterval': '45',
-                     'Compression': 'yes',
-                     'CompressionLevel': '9',
-                     'ForwardX11': 'yes'
-                     }
-config['bitbucket.org'] = {'User': 'Atlan'}
-config['topsecret.server.com'] = {'Host Port': '50022', 'ForwardX11': 'no'}
-with open('message.ini', 'w') as f:
-    config.write(f)
+
+
+def config_write():
+    config["DEFAULT"] = {'first_code': '600733',
+                         'second_code': '600111',
+                         'start_date': '20210801',
+                         'end_date': '20210818'
+                         }
+    config['personal_message'] = {'money': '100000'}
+    config['profit_message'] = {'Host Port': '50022', 'ForwardX11': 'no'}
+    with open('message.ini', 'w') as f:
+        config.write(f)
+
+
+def config_read():
+    config.read('message.ini')
+    return float(config['personal_message']['money'])
+    # config.set('personal_message', 'money', '2222')
+    # with open('message.ini', 'w') as f:
+    #     config.write(f)
+
+
+if __name__ == '__main__':
+    config_write()
+    # config_read()
