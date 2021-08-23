d6e770cd314c3688e80044024d186d64a7dad830
yangjiameng <918021618@qq.com>
2021-08-23 18:18:27 +0800

    配置文件读写模块

--- config_rw.py
+++ config_rw.py
@@ -10,11 +10,11 @@ def config_write():
                          'end_date': '20210818'
                          }
     config['personal_message'] = {'money': '100000'}
-    config['profit_message'] = {'今日盈亏': '4050',
-                                '资产剩余': '110198.82',
-                                '总收益率': '3.5%%',
+    config['profit_message'] = {'今日盈亏': '10127.40',
+                                '资产剩余': '120325.48',
+                                '总收益率': '9.44%%',
                                 '持有公司': '北方稀土',
-                                '持仓占比': '92%%',
+                                '持仓占比': '70%%',
                                 '投资风格': '激进型'}
     with open('message.ini', 'w') as f:
         config.write(f)
