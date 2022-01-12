2c42870ab3a7cdcd9a3aa9cb05820a675d1fc553
yangjiameng <918021618@qq.com>
2022-01-12 15:14:20 +0800

    修复无跌停数据解析错误的bug

--- up_down_stock_list.py
+++ up_down_stock_list.py
@@ -16,7 +16,6 @@ def write_csv(now_time_url, now_time_excel):
     j = json.loads(zt.text[20:len(zt.text) - 2])
     j_dt = json.loads(dt.text[20:len(dt.text) - 2])
     lenth_zt = len(j['data']['pool'])
-    lenth_dt = len(j_dt['data']['pool'])
     wbr = pl.Workbook()
     sheet_0 = wbr.active
     sheet_0.title = '每日涨停股'
@@ -42,20 +41,22 @@ def write_csv(now_time_url, now_time_excel):
     info_dt = ['代码', '名称', '价格/(元)', '涨跌幅/(%)', '成交额/(亿)', '流通市值/(亿)', '总市值/(亿)', '换手率/(%)', '连续跌停',
                '动态市盈率', '最后封板/(h,m)', '封板资金/(亿)', '翘板板数', '板上成交额', '所属行业']
     sheet_1.append(info_dt)
-    for j in range(0, lenth_dt):
-        list_data_dt = [j_dt['data']['pool'][j]['c'], j_dt['data']['pool'][j]['n'], j_dt['data']['pool'][j]['p'] / 1000,
-                        round(j_dt['data']['pool'][j]['zdp'], 2),
-                        round(j_dt['data']['pool'][j]['amount'] / pow(10, 8), 2),
-                        round(j_dt['data']['pool'][j]['ltsz'] / pow(10, 8), 2),
-                        round(j_dt['data']['pool'][j]['tshare'] / pow(10, 8), 2),
-                        round(j_dt['data']['pool'][j]['hs'], 2),
-                        j_dt['data']['pool'][j]['days'],
-                        round(j_dt['data']['pool'][j]['pe'], 2), round(j_dt['data']['pool'][j]['lbt'] / 10000, 2),
-                        round(j_dt['data']['pool'][j]['fund'] / pow(10, 8), 2),
-                        j_dt['data']['pool'][j]['oc'],
-                        round(j_dt['data']['pool'][j]['fba'] / pow(10, 8), 2),
-                        j_dt['data']['pool'][j]['hybk']]
-        sheet_1.append(list_data_dt)
+    if j_dt['data'] is not None:
+        lenth_dt = len(j_dt['data']['pool'])
+        for j in range(0, lenth_dt):
+            list_data_dt = [j_dt['data']['pool'][j]['c'], j_dt['data']['pool'][j]['n'], j_dt['data']['pool'][j]['p'] / 1000,
+                            round(j_dt['data']['pool'][j]['zdp'], 2),
+                            round(j_dt['data']['pool'][j]['amount'] / pow(10, 8), 2),
+                            round(j_dt['data']['pool'][j]['ltsz'] / pow(10, 8), 2),
+                            round(j_dt['data']['pool'][j]['tshare'] / pow(10, 8), 2),
+                            round(j_dt['data']['pool'][j]['hs'], 2),
+                            j_dt['data']['pool'][j]['days'],
+                            round(j_dt['data']['pool'][j]['pe'], 2), round(j_dt['data']['pool'][j]['lbt'] / 10000, 2),
+                            round(j_dt['data']['pool'][j]['fund'] / pow(10, 8), 2),
+                            j_dt['data']['pool'][j]['oc'],
+                            round(j_dt['data']['pool'][j]['fba'] / pow(10, 8), 2),
+                            j_dt['data']['pool'][j]['hybk']]
+            sheet_1.append(list_data_dt)
     print('跌停股已写入完毕！')
     wbr.save('zd/涨跌停股明细' + now_time_excel + '.xlsx')
 
